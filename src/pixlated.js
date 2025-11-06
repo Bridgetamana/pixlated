b96d794e479af1f0baf8c492d70cae028c820eba
Bridgetamana <amanabridget03@gmail.com>
2025-11-06 21:05:33 +0100

    update PixlatedImage canvas context

--- src/pixlated.js
+++ src/pixlated.js
@@ -6,7 +6,7 @@ class PixlatedImage extends HTMLElement {
         this.attachShadow({ mode: 'open' });
 
         this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
 
         if (!this.ctx) {
             console.error('<pixlated-image>: Canvas 2D context not supported');
